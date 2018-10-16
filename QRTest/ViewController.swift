50472fef512f1551fe0e83a752f7fd360ffff6af
Vicente Cantú <vice.00@hotmail.com>
2018-10-16 14:18:26 -0500

    Initial Commit

---
+++ QRTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  QRTest
+//
+//  Created by Vicente Cantu Garcia on 10/16/18.
+//  Copyright © 2018 Vicente Cantu Garcia. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
